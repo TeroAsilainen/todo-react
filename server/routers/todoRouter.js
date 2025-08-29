98dbaf150ab5b6475fe972dae4baf873a3626835
TeroAsilainen <tero_asilainen@hotmail.com>
2025-08-29 10:29:57 +0300

    8 beta

--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -1,9 +1,13 @@
 import { pool } from '../helper/db.js'
 import { auth } from '../helper/auth.js'
 import { Router } from 'express'
+import { getTasks, postTask } from '../controllers/TaskController.js'
+
 
 const router = Router()
 
+router.get("/",getTasks)
+/*
 router.get('/', (req, res, next) => {
     pool.query('SELECT * FROM task', (err, result) => {
         if (err) {
@@ -12,7 +16,10 @@ router.get('/', (req, res, next) => {
         res.status(200).json(result.rows || [] )
     })
 })
+*/
 
+router.post('/create', auth, postTask)
+/*
 router.post('/create', auth, (req, res, next) => {
     const { task } = req.body
 
@@ -28,6 +35,7 @@ router.post('/create', auth, (req, res, next) => {
             res.status(201).json({id: result.rows[0].id, description: task.description})
     })
 })
+*/
 
 router.delete('/delete/:id', auth, (req, res, next) => {
     const { id } = req.params
